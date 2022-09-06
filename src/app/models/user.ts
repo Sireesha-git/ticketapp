e32739b05da8befa5270270e359bf9d935fefad4
Sireesha-git <sireesha.koniki18@gmail.com>
2022-09-06 11:45:02 +0530

    changes

---
+++ src/app/models/user.ts
@@ -0,0 +1,12 @@
+import { Booking } from './booking';
+
+export class User {
+  customerid?: number;
+  email?: string;
+  userName?: string;
+  mobileNumber?: string;
+  password?: string;
+  location?:string;
+  booking?: Booking;
+  constructor() {}
+}
